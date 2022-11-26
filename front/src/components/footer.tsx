e1f82e1a292064be479b6c3bcf89f3456587f0dc
Arttys1 <curie.arnaud.ac@gmail.com>
2022-11-26 09:36:18 +0100

    footer developpement

--- front/src/components/footer.tsx
+++ front/src/components/footer.tsx
@@ -5,7 +5,19 @@ export class Footer extends React.Component {
     render(): React.ReactNode {
         return (
             <footer className="footer_div">
-                <p>TODO Footer</p>
+                <div className="footer_imgs_emplacement">
+                    <img src="./images/esirem_logo.png" alt="logo_esirem"></img>
+                    <img src="./images/logoUB.png" alt="logo_ub"></img>
+                </div>
+                <div className="footer_text_emplacement">
+                    <ul className="footer_list">             
+                        <li className="footer_list_item">Cours</li>               
+                        <li className="footer_list_item">Matières</li>
+                        <li className="footer_list_item">A propos</li>
+                        <li className="footer_list_item">Licence</li>
+                        <li className="footer_list_item">RGPD</li>
+                    </ul>
+                </div>
             </footer>
         );
     }
