97b09f41143e7f9108b5648027743b3d2cb2cbf2
Arttys1 <curie.arnaud.ac@gmail.com>
2022-12-03 10:43:17 +0100

    progress on tips components

--- front/src/components/course_page_components/TipsCourse.tsx
+++ front/src/components/course_page_components/TipsCourse.tsx
@@ -1,10 +1,18 @@
 import React from "react";
-
+import "./tipsCourse.css"
 
 export class TipsCourse extends React.Component {
     render(): React.ReactNode {
         return (
-            <div></div>
+            <div className="tips_course">
+                <p className="tips_course_title">Annecdote</p>
+                <div className="tips_course_emplacement">
+                    <div className="tips_course_circle"><img className="tips_course_icon" src="./images/course_page_icon/undraw_Notify_re_65on.png"></img></div>                    
+                    <div className="tips_course_rectangle">
+                        <p className="tips_course_text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec mi nulla. Etiam euismod tortor quis lorem porttitor dignissim. In et quam ipsum. Nulla facilisi. Morbi commodo tortor non urna consequat, in ullamcorper nunc aliquet.</p>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
--- front/src/components/course_page_components/paragraphCourse.tsx
+++ front/src/components/course_page_components/paragraphCourse.tsx
@@ -3,6 +3,7 @@ import { ParagraphCourseProps } from "../../model/Course_page_models/paragraphCo
 import { SecondaryTitleCourse } from "./secondaryTitleCourse";
 import { TitleCourse } from "./titleCourse";
 import "./paragraphCourse.css"
+import { TipsCourse } from "./TipsCourse";
 
 export class ParagraphCourse extends React.Component<ParagraphCourseProps> {
     render(): React.ReactNode {
@@ -12,6 +13,7 @@ export class ParagraphCourse extends React.Component<ParagraphCourseProps> {
                 {this.props.secondaryTitle && //check if secondaryTitle is defined
                 <SecondaryTitleCourse content={this.props.secondaryTitle}/>}
                 <p className="paragraph_course_text">{this.props.content}</p>
+                <TipsCourse/>
             </div>
         );
     }
