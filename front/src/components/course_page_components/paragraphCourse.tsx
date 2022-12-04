58ab99681bfd833db4248b1c906c261afa1d31b2
Arttys1 <curie.arnaud.ac@gmail.com>
2022-12-04 07:27:31 +0100

    improve TipsCourseModel

--- front/src/components/course_page_components/paragraphCourse.tsx
+++ front/src/components/course_page_components/paragraphCourse.tsx
@@ -2,18 +2,24 @@ import React from "react";
 import { ParagraphCourseProps } from "../../model/Course_page_models/paragraphCourseModel";
 import { SecondaryTitleCourse } from "./secondaryTitleCourse";
 import { TitleCourse } from "./titleCourse";
-import "./paragraphCourse.css"
 import { TipsCourse } from "./TipsCourse";
+import { TipsCourseType } from "../../model/Course_page_models/tipsCourseModel";
+import "./paragraphCourse.css"
 
 export class ParagraphCourse extends React.Component<ParagraphCourseProps> {
     render(): React.ReactNode {
+        let tipsType : TipsCourseType | undefined = this.props.tipsType;
+        if(!tipsType) {
+            tipsType = TipsCourseType.TIPS;
+        }
+
         return (
             <div className="paragraph_course">
                 <TitleCourse content={this.props.title}/>
                 {this.props.secondaryTitle && //check if secondaryTitle is defined
                 <SecondaryTitleCourse content={this.props.secondaryTitle}/>}
                 <p className="paragraph_course_text">{this.props.content}</p>
-                <TipsCourse/>
+                {this.props.tipsContent && <TipsCourse type={tipsType} content={this.props.tipsContent}/>}
             </div>
         );
     }
---
+++ front/src/model/Course_page_models/tipsCourseModel.ts
@@ -0,0 +1,8 @@
+export type TipsCourseProps = {
+    type: TipsCourseType,
+    content: string,
+}
+
+export enum TipsCourseType {
+    TIPS, WARNING, ERROR,
+}
