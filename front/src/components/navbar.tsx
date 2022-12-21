3abefea273d16607e2da51e7608c0b80867ec976
clement-gh <64776760+clement-gh@users.noreply.github.com>
2022-12-21 12:02:44 +0100

    menu responsive

--- front/src/components/navbar.tsx
+++ front/src/components/navbar.tsx
@@ -11,13 +11,37 @@ export class NavBar extends React.Component  {
                         <img className="navbar_esirem_logo" src="./images/esirem_logo.png" alt="logo_esirem"/>
                     </a>
                 </div>
+<<<<<<< Updated upstream
                 <ul className="navbar_list">                    
+=======
+
+                <ul className="navbar_list" >  
+                    <li className="navbar_list_item"><a href="./connexion">Connexion</a></li>                  
+>>>>>>> Stashed changes
                     <li className="navbar_list_item"><a href="./matieres">Matières</a></li>
                     <li className="navbar_list_item"><a href="./cours">Cours</a></li>
                     <li className="navbar_list_item"><a href="./exercices">Exercices</a></li>
                     <li className="navbar_list_item"><a href="./about">A propos</a></li>
                 </ul>
-            </div>
+                <div className="dropdown">
+                
+                    
+                    <img src="./images/menu.png" alt="Cinque Terre" width="50" height="50"></img>
+                        <div className="dropdown-content">
+        
+                            <a href="./connexion">Connexion</a>
+                            <a href="./matieres">Matières</a>
+                            <a href="./cours">Cours</a>
+                            <a href="./exercices">Exercices</a>
+                            <a href="./about">A propos</a>
+            
+                        </div>
+                   
+                </div>
+
+
+             </div>
+
         );
     }
 
