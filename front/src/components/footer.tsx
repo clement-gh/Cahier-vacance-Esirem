59c2b4333e0a1333686ceaf07911420b6769c715
clement-gh <64776760+clement-gh@users.noreply.github.com>
2022-12-23 11:36:25 +0100

    un peu plus responsive sur le footer
    (c'est pas encore ça)

--- front/src/components/footer.tsx
+++ front/src/components/footer.tsx
@@ -6,14 +6,12 @@ export class Footer extends React.Component {
         return (
             <footer className="footer_div">
                 <div className="footer_imgs_emplacement">
-                    <a href="https://esirem.u-bourgogne.fr/"><img src="./images/esirem_logo.png" alt="logo_esirem"></img></a>
-                    <a href="https://www.u-bourgogne.fr/"><img src="./images/logoUB.png" alt="logo_ub"></img></a>
+                    <a className="UB" href="https://esirem.u-bourgogne.fr/"><img src="./images/esirem_logo.png" alt="logo_esirem"></img></a>
+                    <a className="ESIREM"href="https://www.u-bourgogne.fr/"><img src="./images/logoUB.png" alt="logo_ub"></img></a>
                 </div>
                 <div className="footer_text_emplacement">
                     <ul className="footer_list">             
-                        <li className="footer_list_item"><a href="/cours">Cours</a></li>               
-                        <li className="footer_list_item"><a href="/matieres">Matières</a></li>
-                        <li className="footer_list_item"><a href="/about">A propos</a></li>
+                       
                         <li className="footer_list_item"><a href="/licence">Licence</a></li>
                         <li className="footer_list_item"><a href="/rgpd">RGPD</a></li>
                     </ul>
