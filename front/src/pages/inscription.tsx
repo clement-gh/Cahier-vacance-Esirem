5521bf7cac9eb866f5b7d966c0fd56d8a0a7f5b1
clement-gh <64776760+clement-gh@users.noreply.github.com>
2023-01-03 11:00:06 +0100

    Update inscription.tsx

--- front/src/pages/inscription.tsx
+++ front/src/pages/inscription.tsx
@@ -33,7 +33,18 @@ export class Inscription extends React.Component {
                 <NavBar/>
                 <Title content="Inscription"/>
                 <div className="inscription_form">
-                    <form>
+                    <form
+                    onSubmit={(e: React.SyntheticEvent) => {
+                        e.preventDefault();
+                        const target = e.target as typeof e.target & {
+                          email: { value: string };
+                          password: { value: string };
+                        };
+                        const email = target.email.value; // typechecks!
+                        const password = target.password.value; // typechecks!
+                        
+                      }}
+                    >
                         <label>
                             <input type="text" name="userName" placeholder="Nom d'utilisateur" />
                         </label>
@@ -48,6 +59,29 @@ export class Inscription extends React.Component {
                         </label>
                         <label>
                             <input type="text" name="confirmMail" placeholder="Confirmer l'adresse mail" />
+                        </label>
+                       
+                        
+                        <label className="custom-checkbox-button_provenance">
+                            <h2>Je viens de:</h2> 
+                            <input type="checkbox" className="custom-checkbox-button_iut" />
+                            <span className="custom-checkbox-button__label">IUT</span>
+                            <input type="checkbox" className="custom-checkbox-button_prepa" />
+                            <span className="custom-checkbox-button__label">Prépa</span>
+                            <input type="checkbox" className="custom-checkbox-button_prepa_integree" />
+                            <span className="custom-checkbox-button__label">Prépa intégrée</span>
+                            <input type="checkbox" className="custom-checkbox-button_licence" />
+                            <span className="custom-checkbox-button__label">Licence</span>
+                            <input type="checkbox" className="custom-checkbox-button_autre" />
+                            <span className="custom-checkbox-button__label">Autre</span>
+                        </label>
+
+                        <label className="custom-checkbox-button_niveau">
+                            <h2>Je rentre en:</h2>
+                            <input type="checkbox" className="custom-checkbox-button_1A" />
+                            <span className="custom-checkbox-button__label">1A</span>
+                            <input type="checkbox" className="custom-checkbox-button_3A" />
+                            <span className="custom-checkbox-button__label">3A</span>
                         </label>
                         
                     </form>
