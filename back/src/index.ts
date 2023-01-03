ec17f11fd90577459082ce0af7f4495edf0cf613
Arttys1 <curie.arnaud.ac@gmail.com>
2023-01-03 18:50:42 +0100

    correct import and file transfer problems

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,2 +1,36 @@
-import fastify from 'fastify';
+import fastify, { FastifyReply, FastifyRequest } from 'fastify';
 import cors from '@fastify/cors';
+import { REPL_MODE_SLOPPY } from 'repl';
+
+//importation des differentes tables
+import coursImp from './cours';
+import quizzImp from './quizz';
+import exoLongImp from './exolong';
+import anneeImp from './annee';
+
+const server = require('fastify')()
+
+server.register(require('@fastify/mysql'), {
+  connectionString: 'mysql://root@localhost/Projet_Esirem'
+})
+
+server.register(cors,{});
+
+server.register(coursImp);
+server.register(quizzImp);
+server.register(exoLongImp);
+server.register(anneeImp);
+
+server.get('/', function(request: FastifyRequest, reply: FastifyReply) {
+    server.mysql.query(
+      'SELECT * FROM `cours`',
+      function onResult (err:any, result:any) {
+        reply.send(err || result)
+      }
+    )
+})
+
+server.listen({ port: 4000 }, (err:any, address:any) => {
+    if (err) throw err
+    console.log(`server listening on ` + address)
+  })
