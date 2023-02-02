5c1a485e20ffd3c18e605d54897bc4fc1fd393f2
Arttys1 <curie.arnaud.ac@gmail.com>
2023-02-02 16:49:13 +0100

    create a beautiful signup page

--- front/src/pages/inscription.tsx
+++ front/src/pages/inscription.tsx
@@ -1,26 +1,13 @@
-import React, { createRef } from "react";
-import "./inscription.css";
-
+import React from "react";
 import { NavBar } from "../components/navbar";
 import { Title } from "../components/title";
-type State = {
-    text: string;
-  };
+import { Footer } from "../components/footer";
+import { Input } from "../components/form/input";
+import { RadioBoxList } from "../components/form/radioBoxList";
+import { Button } from "../components/button";
+import "./inscription.css";
+
 export class Inscription extends React.Component {
-    
-    state = {
-        text: "",
-      };
-    private txtMail = createRef<HTMLInputElement>();
-
-    handleChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
-        this.setState({text: e.currentTarget.value})
-      }
-
-    processForm()
-    {
-        console.log("Go !")
-    }
       
     render(){
         return (
@@ -29,50 +16,31 @@ export class Inscription extends React.Component {
                 <NavBar/>
                 <Title content="Inscription"/>
                 <div className="inscription_form">
-                    <form >
-                        <label>
-                            <input type="text" value={this.state.text} name="userName" placeholder="Nom d'utilisateur" />
-                        </label>
-                        <label>
-                            <input type="password" name="password" placeholder="Mot de passe" />
-                        </label>
-                        <label>
-                            <input type="password" name="confirmPassword" placeholder="Confirmer le mot de passe" />
-                        </label>
-                        <label>
-                            <input type="text" name="mail" placeholder="Adresse mail" ref={this.txtMail} />
-                        </label>
-                        <label>
-                            <input type="text" name="confirmMail" placeholder="Confirmer l'adresse mail" />
-                        </label>
-                       
+                    <form className="form_signup">
+                        <Input label="Nom"/>
+                        <Input label="Prénom"/>
+                        <Input label="Mail"/>
+                        <Input label="Mot de passe" type="password"/>
+                        <Input label="Confirmer le mot de passe" type="password"/>
                         
-                        <label className="custom-checkbox-button_provenance">
-                            <h2>Je viens de:</h2> 
-                            <input type="checkbox" className="custom-checkbox-button_iut" />
-                            <span className="custom-checkbox-button__label">IUT</span>
-                            <input type="checkbox" className="custom-checkbox-button_prepa" />
-                            <span className="custom-checkbox-button__label">Prépa</span>
-                            <input type="checkbox" className="custom-checkbox-button_prepa_integree" />
-                            <span className="custom-checkbox-button__label">Prépa intégrée</span>
-                            <input type="checkbox" className="custom-checkbox-button_licence" />
-                            <span className="custom-checkbox-button__label">Licence</span>
-                            <input type="checkbox" className="custom-checkbox-button_autre" />
-                            <span className="custom-checkbox-button__label">Autre</span>
-                        </label>
+                        <RadioBoxList label="Je rentre en :" radioBoxOptions={[
+                            {name: "1ère année prépa intégrée"},
+                            {name: "1ère année cycle ingénieur"},
+                        ]}/>
 
-                        <label className="custom-checkbox-button_niveau">
-                            <h2>Je rentre en:</h2>
-                            <input type="checkbox" className="custom-checkbox-button_1A" />
-                            <span className="custom-checkbox-button__label">1A</span>
-                            <input type="checkbox" className="custom-checkbox-button_3A" />
-                            <span className="custom-checkbox-button__label">3A</span>
-                        </label>
-                        <button type="button" onClick={()=>{this.processForm()}}>Envoyer</button>
+                        <RadioBoxList label="Je viens de :" radioBoxOptions={[
+                            {name: "IUT"},
+                            {name: "Prépa"},
+                            {name: "GEIPI"},
+                            {name: "Licence"},
+                            {name: "Autre"},
+                        ]}/>
+                        <Button content="Envoyer"/>
                     </form>
                         
                 </div>
             </div>
+            <Footer/>
         </main>
         );
     }
