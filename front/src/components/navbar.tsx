da11c08fbd92691ecba0ede943463ee8e2681c6b
clement-gh <64776760+clement-gh@users.noreply.github.com>
2023-02-05 09:25:35 +0100

    Update Menu

--- front/src/components/navbar.tsx
+++ front/src/components/navbar.tsx
@@ -2,7 +2,10 @@ import React from "react";
 import "./navbar.css"
 
 export class NavBar extends React.Component  {
-
+/**<li className="navbar_list_item"><a href="/cours">Cours</a></li>
+<li className="navbar_list_item"><a href="/exercices">Exercices</a></li> 
+                    <a href="/cours">Cours</a>
+                    <a href="/exercices">Exercices</a>*/
     render(): React.ReactNode {
         return (
             <div className="navbar">
@@ -13,22 +16,19 @@ export class NavBar extends React.Component  {
                 </div>
 
                 <ul className="navbar_list" >  
-                    <li className="navbar_list_item"><a href="/connexion">Connexion</a></li>                  
                     <li className="navbar_list_item"><a href="/matieres">Matières</a></li>
-                    <li className="navbar_list_item"><a href="/cours">Cours</a></li>
-                    <li className="navbar_list_item"><a href="/exercices">Exercices</a></li>
+                    <li className="navbar_list_item"><a href="/connexion">Connexion</a></li>                 
                     <li className="navbar_list_item"><a href="/about">A propos</a></li>
                 </ul>
                 <div className="dropdown">
                 
                     
-                    <img src="./images/menu.png" alt="Cinque Terre" width="50" height="50"></img>
+                    <img src="./images/menu.png" alt="menu" width="50" height="50"></img>
                         <div className="dropdown-content">
-        
-                            <a href="/connexion">Connexion</a>
+
                             <a href="/matieres">Matières</a>
-                            <a href="/cours">Cours</a>
-                            <a href="/exercices">Exercices</a>
+                            <a href="/connexion">Connexion</a>
+                          
                             <a href="/about">A propos</a>
             
                         </div>
