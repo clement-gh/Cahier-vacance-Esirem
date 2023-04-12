b9686572225e1613327d410e6d95f1663727d174
Arttys1 <curie.arnaud.ac@gmail.com>
2023-04-12 09:21:07 +0200

    Correct backoffice menu link

--- front/src/pages/backoffice/menu_backoffice.tsx
+++ front/src/pages/backoffice/menu_backoffice.tsx
@@ -15,11 +15,11 @@ export class MenuBackOffice extends React.Component {
                     <p className="menu_backoffice_page_text">Que souhaitez-vous faire ?</p>
 
                     <section className="menu_backoffice_blocs_courses">
-                        <BlocCourse title="Gestion des utilisateurs" link="/backoffice/Gestion_utilisateur" imgAdd="./images/pexels-photo-459653.jpeg" imgAlt="background_bloc_informatique" iconAdd="./images/computer.png"iconAlt="computer_icon"/>
-                        <BlocCourse title="Gestion Cours/Exercices" link="/backoffice/Gestion_cours" imgAdd="./images/background_maths.png" imgAlt="background_bloc_maths" iconAdd="./images/logoMaths.png"iconAlt="maths_icon"/>
-                        <BlocCourse title="Création d'un cours" link="/backoffice/creation_cours" imgAdd="./images/background_elec.png" imgAlt="background_bloc_informatique" iconAdd="./images/logoElectronique.png"iconAlt="elec_icon"/>
-                        <BlocCourse title="Création d'un exercice" link="/backoffice/creation_exercice" imgAdd="./images/pexels-photo-459653.jpeg" imgAlt="background_bloc_informatique" iconAdd="./images/computer.png"iconAlt="computer_icon"/>
-                        <BlocCourse title="Création d'un quizz" link="/backoffice/creation_quizz" imgAdd="./images/background_maths.png" imgAlt="background_bloc_maths" iconAdd="./images/logoMaths.png"iconAlt="maths_icon"/>
+                        <BlocCourse title="Gestion des utilisateurs" link="/backoffice/gestion_utilisateurs" imgAdd="/images/pexels-photo-459653.jpeg" imgAlt="background_bloc_informatique" iconAdd="/images/computer.png"iconAlt="computer_icon"/>
+                        <BlocCourse title="Gestion Cours/Exercices" link="/backoffice/gestion_cours" imgAdd="/images/background_maths.png" imgAlt="background_bloc_maths" iconAdd="/images/logoMaths.png"iconAlt="maths_icon"/>
+                        <BlocCourse title="Création d'un cours" link="/backoffice/creation_cours" imgAdd="/images/background_elec.png" imgAlt="background_bloc_informatique" iconAdd="/images/logoElectronique.png"iconAlt="elec_icon"/>
+                        <BlocCourse title="Création d'un exercice" link="/backoffice/creation_exercice" imgAdd="/images/pexels-photo-459653.jpeg" imgAlt="background_bloc_informatique" iconAdd="/images/computer.png"iconAlt="computer_icon"/>
+                        <BlocCourse title="Création d'un quizz" link="/backoffice/creation_quizz" imgAdd="/images/background_maths.png" imgAlt="background_bloc_maths" iconAdd="/images/logoMaths.png"iconAlt="maths_icon"/>
                     </section>
 
 
