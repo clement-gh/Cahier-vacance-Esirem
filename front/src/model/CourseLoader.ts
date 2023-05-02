69415c6598e3e481a74fac560729366a70ceb7b5
Arttys1 <curie.arnaud.ac@gmail.com>
2023-05-02 18:13:34 +0200

    remove comment code

--- front/src/model/CourseLoader.ts
+++ front/src/model/CourseLoader.ts
@@ -19,30 +19,12 @@ export type Paragraph = {
 //function to load a course with his id
 export async function loadCourse(id: number | string): Promise<Course> {
     let principal = await callAPI("cours/" + id);
-    let contenu:string = principal.contenu;
-    console.log(contenu);
-    //parse each bloc tag
-    // let paragraphs: Paragraph[] = []
-    // let blocsContent = secondary.texte.split("<bloc>");
-    // for(let i = 1; i < blocsContent.length; i++) {
-    //     //extract sub string from the bloc
-    //     let title = extractStringBetweenBorn(blocsContent[i], "<titre>", "</titre>");
-    //     let subTitle = extractStringBetweenBorn(blocsContent[i], "<soustitre>", "</soustitre>");
-    //     let content = extractStringBetweenBorn(blocsContent[i], "<contenu>", "</contenu>");
-    //     let annotation = extractStringBetweenBorn(blocsContent[i], "<annotation>", "</annotation>");
-    //     paragraphs.push({
-    //         title,
-    //         subTitle,
-    //         content,
-    //         annotation,
-    //     });
-    // }
     
     let course : Course = {
         id: principal.idCours,
         idRubrique: principal.idRubrique,
         title: principal.titreCours,
-        contenu: contenu,
+        contenu: principal.contenu,
     }
     return course;
 }
